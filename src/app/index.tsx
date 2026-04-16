d56a04c9dc036d038f947339d4696199108235d4
Yasa Zaheen <yasazaheen@gmail.com>
2026-04-16 19:52:27 -0400

    some more clean up before trying native wind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -50,24 +50,4 @@ const styles = StyleSheet.create({
     paddingBottom: BottomTabInset + Spacing.three,
     maxWidth: MaxContentWidth,
   },
-  heroSection: {
-    alignItems: "center",
-    justifyContent: "center",
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
-  },
-  title: {
-    textAlign: "center",
-  },
-  code: {
-    textTransform: "uppercase",
-  },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: "stretch",
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
-  },
 });
--- src/app/settings.tsx
+++ src/app/settings.tsx
@@ -1,10 +1,24 @@
+import { BottomTabInset, MaxContentWidth, Spacing } from "@/constants/theme";
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
 const Settings = () => {
   return (
-    <View>
-      <Text>Settings</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View>
+        <Text>Settings</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 export default Settings;
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    paddingHorizontal: Spacing.four,
+    alignItems: "center",
+    gap: Spacing.three,
+    paddingBottom: BottomTabInset + Spacing.three,
+    maxWidth: MaxContentWidth,
+  },
+});
